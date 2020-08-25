457f2b7231c254369f337888b74bef40a7749b29
Mekyle <mekylenaidoo@yahoo.com>
2020-08-25 14:10:40 +0800

    Update
    Added all the details

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Tailwindcss from '../components/Tailwindcss.svelte';
+</script>
+
+<style>
+	main {
+		background-color: white;
+	}
+</style>
+
+<Tailwindcss />
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,35 @@
+<style xmlns="http://www.w3.org/1999/html">
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+  }
+  
+  .landing-image{
+    width: 100%;
+    height: 100%;
+  }
+</style>
+
+<svelte:head>
+	<title>OnlySimps</title>
+</svelte:head>
+
+<!--<h1>Great success!</h1>-->
+
+    <div class="flex flex-wrap md items-center h-screen">
+     <div class="bg-red-600 w-full md:w-1/2 h-screen">
+        <img
+          src="https://www.linkpicture.com/q/542f46db-51d7-4cc3-95ee-c2fefbdbeee8.jpg"
+          class="h-screen w-full landing-image"
+          alt=""
+        />
+      </div>
+      <div class="bg-white w-full md:w-1/2 h-screen">
+        <h1>OnlySimps</h1>
+        
+      </div>
+    </div>
+
+<!--<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>-->
